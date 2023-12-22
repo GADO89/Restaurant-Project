8cbc2a6e0ef8d20a188e4fefe55b647d72e4fe5e
GADO89 <gadomahmoud377@gmail.com>
2023-12-22 04:31:25 +0200

    set Annotation in Models

--- src/main/java/com/spring/restaurant/model/BaseEntity.java
+++ src/main/java/com/spring/restaurant/model/BaseEntity.java
@@ -3,10 +3,14 @@ package com.spring.restaurant.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BaseEntity {
+    @Column( name = "id")
     private Long id;
 
 }
