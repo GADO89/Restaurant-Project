69293e552cad9b0d3e316f314214bf5d49249fc8
GADO89 <gadomahmoud377@gmail.com>
2023-12-22 07:10:41 +0200

    add configuration of sql and testing

--- src/main/java/com/spring/restaurant/model/Category.java
+++ src/main/java/com/spring/restaurant/model/Category.java
@@ -1,6 +1,5 @@
 package com.spring.restaurant.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,7 +8,6 @@ import javax.persistence.Table;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "category")
 public class Category extends CategoryOrder {
