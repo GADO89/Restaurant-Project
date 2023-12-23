e17e6d5e12c0cc934734120f1bf2e496f3dbd0c5
GADO89 <gadomahmoud377@gmail.com>
2023-12-23 06:53:53 +0200

    Orders find by category id(Controller)

--- src/main/java/com/spring/restaurant/controller/OrderController.java
+++ src/main/java/com/spring/restaurant/controller/OrderController.java
@@ -5,9 +5,7 @@ import com.spring.restaurant.model.Order;
 import com.spring.restaurant.service.CategoryService;
 import com.spring.restaurant.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,11 @@ public class OrderController {
     public List<Order> allOrders(){
         return   orderService.getAllOrders();
     }
+
+    @GetMapping("/api/category")
+    public List<Order> getAllOrderByIdCategories(@RequestParam Long id){
+        return orderService.getOrdersByIdCategories(id);
+    }
+
 
 }
