4990876315b8a7aac7a4849d77b061e9c03196c3
GADO89 <gadomahmoud377@gmail.com>
2023-12-23 04:40:34 +0200

    allCategories Controller-Service-Repository

--- src/main/java/com/spring/restaurant/controller/CategoryController.java
+++ src/main/java/com/spring/restaurant/controller/CategoryController.java
@@ -1,16 +1,24 @@
 package com.spring.restaurant.controller;
 
+import com.spring.restaurant.model.Category;
 import com.spring.restaurant.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping()
+//@RequestMapping("api")
 public class CategoryController {
     private CategoryService categoryService;
     @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+    @GetMapping("/api/allCategories")
+    public List<Category> getAllCategories(){
+        return   categoryService.allCategories();
     }
 }
