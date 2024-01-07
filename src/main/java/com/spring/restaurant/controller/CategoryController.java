83cdb339ece2ec66325e9cf5904ea36128fbae4e
GADO89 <gadomahmoud377@gmail.com>
2024-01-07 19:42:43 +0200

    Create PurchaseController

--- src/main/java/com/spring/restaurant/controller/CategoryController.java
+++ src/main/java/com/spring/restaurant/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.spring.restaurant.controller;
 import com.spring.restaurant.model.Category;
 import com.spring.restaurant.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("/api")
 public class CategoryController {
     private CategoryService categoryService;
---
+++ src/main/java/com/spring/restaurant/controller/PurchaseController.java
@@ -0,0 +1,24 @@
+package com.spring.restaurant.controller;
+
+import com.spring.restaurant.dto.PurchaseRequest;
+import com.spring.restaurant.dto.PurchaseResponse;
+import com.spring.restaurant.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:4200")
+@RequestMapping("/api/buy")
+public class PurchaseController {
+
+    private PurchaseService purchaseService;
+
+    @Autowired
+    public PurchaseController(PurchaseService purchaseService) {
+        this.purchaseService = purchaseService;
+    }
+    @PostMapping("/purchase")
+    public PurchaseResponse addRequestOrder(@RequestBody PurchaseRequest purchaseRequest){
+       return purchaseService.addRequestOrder(purchaseRequest);
+    }
+}
