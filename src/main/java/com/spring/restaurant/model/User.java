9778d35ff6e0e1d6006bb2313ed17bb1e43a1651
GADO89 <gadomahmoud377@gmail.com>
2024-01-09 01:38:49 +0200

    set Annotations model (User - Authorities)

--- src/main/java/com/spring/restaurant/model/User.java
+++ src/main/java/com/spring/restaurant/model/User.java
@@ -1,8 +1,30 @@
 package com.spring.restaurant.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class User extends BaseEntity{
 
-    private String roleName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "active")
+    private int active;
 
 }
