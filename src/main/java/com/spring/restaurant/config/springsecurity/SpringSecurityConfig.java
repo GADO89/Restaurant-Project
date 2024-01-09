eb269487511266eb116ed0accc0e84e30430f49a
GADO89 <gadomahmoud377@gmail.com>
2024-01-10 00:17:49 +0200

    UserService

--- src/main/java/com/spring/restaurant/config/springsecurity/SpringSecurityConfig.java
+++ src/main/java/com/spring/restaurant/config/springsecurity/SpringSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.spring.restaurant.config.springsecurity;
 
+import com.spring.restaurant.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -12,9 +14,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserService userService;
+    @Autowired
+    public SpringSecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-     auth.authenticationProvider()
+     auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
@@ -30,12 +39,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     DaoAuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        daoAuthenticationProvider.setUserDetailsService();
+       daoAuthenticationProvider.setUserDetailsService(userService);
         return daoAuthenticationProvider;
     }
 
     @Bean
     PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder()
+        return new BCryptPasswordEncoder();
     }
 }
