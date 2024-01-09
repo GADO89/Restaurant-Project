fc97e14b5600f98e4aa86e5898ecc32c5a13bedb
GADO89 <gadomahmoud377@gmail.com>
2024-01-09 04:18:33 +0200

    Create UserRepository

--- src/main/java/com/spring/restaurant/config/DataRestApiConfig.java
+++ src/main/java/com/spring/restaurant/config/DataRestApiConfig.java
@@ -2,6 +2,7 @@ package com.spring.restaurant.config;
 
 import com.spring.restaurant.model.Category;
 import com.spring.restaurant.model.Order;
+import com.spring.restaurant.model.User;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -22,6 +23,8 @@ public class DataRestApiConfig implements RepositoryRestConfigurer {
         HttpMethod [] preventMethod={HttpMethod.GET,HttpMethod.POST,HttpMethod.PUT,HttpMethod.DELETE};
         disableHttpMethod(Category.class,config,preventMethod);
         disableHttpMethod(Order.class,config,preventMethod);
+        disableHttpMethod(User.class,config,preventMethod);
+
 
        /* HttpMethod [] preventMethod={HttpMethod.GET,HttpMethod.POST,HttpMethod.PUT,HttpMethod.DELETE};
         config.getExposureConfiguration()
